dffbc6f485e8baf67fd373711854c453c81ca7ce
flaggs_hwangmansuk <78951135+hwangmansuk@users.noreply.github.com>
2022-05-25 17:26:17 +0900

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
 const API_KEY = 'API キーを入力してください';
-const SHEET_ID = 'Google スプレッドシートの Sheet ID を入力してください';
+const SHEET_ID = 'https://docs.google.com/spreadsheets/d/1e1QYdfvnFoyj5JMd_iDb0G3be7WUBiYADcFPpMqvfE4/edit?usp=sharing';
