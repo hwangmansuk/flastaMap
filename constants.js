1468ff64668c2b218bd5e238debb412ae60cc17e
flaggs_hwangmansuk <78951135+hwangmansuk@users.noreply.github.com>
2022-05-27 17:48:32 +0900

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 const MAP_KEY = 'AIzaSyDiNcso1WGDrr_FNbeq6jnL0qISN1w8wJI';
 const SHEET_KEY = 'AIzaSyBrsoOSRgTyUTXzDkt6XFwxPl-4sSeTpPg';
-const SHEET_ID = '13SXU0KYLsx68Zkemz-be-OgGaC6ZUKC-mRtjKX1TzI4';
-const SHEET_SRC = 'https://docs.google.com/spreadsheets/d/13SXU0KYLsx68Zkemz-be-OgGaC6ZUKC-mRtjKX1TzI4/edit?usp=sharing';
+const SHEET_ID = '1WFU9Hr3onnALj_RyKcsU8KaMeQAO7kuowejuV8F0fXU';
+const SHEET_SRC = 'https://docs.google.com/spreadsheets/d/1WFU9Hr3onnALj_RyKcsU8KaMeQAO7kuowejuV8F0fXU/edit?usp=sharing';
