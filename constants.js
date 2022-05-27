db00fb6d5c86087198c60995f52f72fd1db8f097
flaggs_hwangmansuk <78951135+hwangmansuk@users.noreply.github.com>
2022-05-27 15:56:28 +0900

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,3 @@
-const API_KEY = 'AIzaSyBrsoOSRgTyUTXzDkt6XFwxPl-4sSeTpPg';
+const MAP_KEY = 'AIzaSyDiNcso1WGDrr_FNbeq6jnL0qISN1w8wJI';
+const SHEET_KEY = 'AIzaSyBrsoOSRgTyUTXzDkt6XFwxPl-4sSeTpPg';
 const SHEET_ID = '1e1QYdfvnFoyj5JMd_iDb0G3be7WUBiYADcFPpMqvfE4';
