2d54cb093513ce672f2da5434b3ff8c10634015a
flaggs_hwangmansuk <78951135+hwangmansuk@users.noreply.github.com>
2022-07-13 20:09:42 +0900

    update key

--- constants.js
+++ constants.js
@@ -2,3 +2,4 @@ const MAP_KEY = 'AIzaSyDiNcso1WGDrr_FNbeq6jnL0qISN1w8wJI';
 const SHEET_KEY = 'AIzaSyBrsoOSRgTyUTXzDkt6XFwxPl-4sSeTpPg';
 const SHEET_ID = '1WFU9Hr3onnALj_RyKcsU8KaMeQAO7kuowejuV8F0fXU';
 const SHEET_SRC = 'https://docs.google.com/spreadsheets/d/1WFU9Hr3onnALj_RyKcsU8KaMeQAO7kuowejuV8F0fXU/edit?usp=sharing';
+const MASTER_KEY ='AIzaSyBq_w6hbkRRqmuCC_PrO01OCjF2wozJbY8';
